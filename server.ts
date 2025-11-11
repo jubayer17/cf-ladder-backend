3b5d2d0eacad9d53b937493ab4d595cc11430fb8
jubayer17 <jubayer17@cse.pstu.ac.bd>
2025-11-11 12:37:52 +0600

    Update CORS allowedHeaders to include additional headers for improved request handling

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Cache-Control', 'Pragma', 'Expires'],
     credentials: true
 }));
 
