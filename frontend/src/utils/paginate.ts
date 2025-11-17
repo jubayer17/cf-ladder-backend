52e0d4781150cbe8e464fa19883c65a1f183015a
jubayer17 <jubayer17@cse.pstu.ac.bd>
2025-11-17 23:02:45 +0600

    chore: Remove frontend folder from backend repo (separate repository)

--- frontend/src/utils/paginate.ts
+++
@@ -1,5 +0,0 @@
-// utils/paginate.ts
-export function paginate<T>(items: T[], page: number, perPage: number): T[] {
-    const start = (page - 1) * perPage;
-    return items.slice(start, start + perPage);
-}
